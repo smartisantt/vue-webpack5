1acc223e65a06c3438da50885816acadf97e4dbb
allen <ququ250@qq.com>
2021-12-01 01:22:34 +0800

    配置webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader-plugin');
+
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -7,4 +10,27 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
   },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      _: __dirname,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader',
+      },
+      { test: /\.js$/, use: 'babel-loader' },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new VueLoaderPlugin(),
+  ],
 };
