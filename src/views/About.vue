4d4278eb839570123d69e08fc472ecab6ee7795a
Chenlunju <ququ250@qq.com>
2021-12-03 19:56:04 +0800

    add vuex

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,19 @@
 <template>
-  <h1>about</h1>
+  <div>
+    <h1>{{$store.state.count}}</h1>
+    <button @click="handleClick" }>click</button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    handleClick() {
+      this.$store.commit('increment')
+    },
+  },
 }
 </script>
 
 <style>
-
 </style>
